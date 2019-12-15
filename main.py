536d15025d9acd4a72c4c03abd98ad6c677f66f3
kyle <williams.kyle@gmx.com>
2019-12-15 13:01:31 -0600

    Use parameter to set max filtration
    There is a parameter to the simplex_tree constructor that allows
    the max filtration value, removing the need to
     1. Filter out edges before adding them to graph
     2. Check if simplices are holes
    
    Also removes unused parameter in is_hole

--- main.py
+++ main.py
@@ -6,49 +6,43 @@ import time
 
 from matplotlib.animation import FuncAnimation
 
-
-def is_hole(graph, points, simplices):
-    # Alpha shape is all points at boundary (this can be difined in initial setup.
-    alpha_shape = [i for (i, p) in enumerate(points) if at_boundary(p)]
-    if set(alpha_shape).issubset(set(graph.nodes())): return False
-    if set(graph.nodes()) in simplices: return False
-
-    return True
-    # A hole cannot contatin the boundary, or be a simplex
-    # return not ( set(alpha_shape).issubset(set(graph.nodes())) or graph.order() == 3)
-
-
 filename = 'initial_data.csv'
 
 with open(filename) as file:
     mypoints = [tuple(map(float, line.strip("\n").split(","))) for line in file]
 
+alpha_shape = [i for (i, p) in enumerate(mypoints) if at_boundary(p)]
+
+
+def is_hole(graph):
+    # A hole cannot contain the boundary, or be a simplex
+    return not (set(alpha_shape).issubset(set(graph.nodes())) or graph.order() == 3)
+
 
 def update(i):
-    global mypoints # USe same points defined outside of function.
-    mypoints = update_position(mypoints) # update via brownina motian
+    global mypoints  # Use same points defined outside of function.
+    mypoints = update_position(mypoints)  # update via brownian motion
 
     # get alpha_complex
     alpha_complex = gudhi.AlphaComplex(mypoints)
 
-    # Note, you can you alpha_square as input??
-    # max_alpha_square = 0.04 ??
-    simplex_tree = alpha_complex.create_simplex_tree()
+    sensing_radius = 0.015
+    max_alpha_square = 2*sensing_radius
+    simplex_tree = alpha_complex.create_simplex_tree(max_alpha_square)
 
-    # get edges with filtration <= 0.04
-    edges = [ simplex[0] for simplex in simplex_tree.get_skeleton(1) if len(simplex[0]) == 2 and simplex[1] <= 0.04]
+    # Get 1-simplices as edges for graph
+    edges = [simplex for (simplex, _) in simplex_tree.get_skeleton(1) if len(simplex) == 2]
 
-    simplices = [set(simplex[0]) for simplex in simplex_tree.get_skeleton(2) if len(simplex[0]) == 3]
     # Use Edges to create graph
     G = nx.Graph()
-    G.add_nodes_from(range(len(mypoints))) #nodes numbered 0 though N points -1
+    G.add_nodes_from(range(len(mypoints)))  # nodes numbered 0 though N points -1
     G.add_edges_from(edges)
 
     # Use Graph and points to create cmap.
     cmap = CMap(G, mypoints)
 
     # filter out all cycles that are not holes(see function at top)
-    holes = list(filter(lambda cycle:is_hole(cycle, mypoints, simplices), boundary_cycle_nodes(cmap)))
+    holes = list(filter(lambda cycle: is_hole(cycle), boundary_cycle_nodes(cmap)))
     
     # plot graph, and replot each hole but in red (since holes will be a list of graphs)
     ax.clear()
