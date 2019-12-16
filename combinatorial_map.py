b5c6fcf8036a0ff0f58522160e3856b393416cff
kyle <williams.kyle@gmx.com>
2019-12-16 00:57:44 -0600

    Clean up formatting

--- combinatorial_map.py
+++ combinatorial_map.py
@@ -3,21 +3,22 @@
 
 # Note that networkx does support planar-graph
 # https://networkx.github.io/documentation/stable/reference/algorithms/planarity.html
-# However, it appeaers that construction is more mannual,
-# and that rotation information is stored as nodes/edges are added to the graph. 
+# However, it appears that construction is more manual,and that rotation information is
+# stored as nodes/edges are added to the graph.
 
 import networkx as nx
 from matplotlib import pyplot as plt
-from math import atan2
+from math import *
+
 
 # Sort counter-clockwise w.r.t center (Thanks to Nick Closuit for idea)
 def theta(a, center):
-    OA = (a[0] - center[0], a[1] - center[1])
-    return atan2(OA[1], OA[0])
+    oa = (a[0] - center[0], a[1] - center[1])
+    return atan2(oa[1], oa[0])
 
 
-class CMap :
-    def __init__(self, Graph, points):
+class CMap:
+    def __init__(self, graph, points):
         self.sorted_edges = dict()
         self.sorted_neighbors = dict()
         self.sorted_darts = dict()
@@ -25,39 +26,37 @@ class CMap :
         self.edge2dart = dict()
         self.dart2edge = dict()
 
-        for i, edge in enumerate(Graph.edges()):
-            self.edge2dart[edge] = 2*i
-            self.edge2dart[(edge[1], edge[0])] = 2*i + 1
+        for n, edge in enumerate(graph.edges()):
+            self.edge2dart[edge] = 2*n
+            self.edge2dart[(edge[1], edge[0])] = 2*n + 1
 
-            self.dart2edge[2*i] = edge
-            self.dart2edge[2*i+1] = (edge[1], edge[0])
-            
+            self.dart2edge[2*n] = edge
+            self.dart2edge[2*n+1] = (edge[1], edge[0])
 
-        for node in Graph.nodes():
-            neighbors = list(Graph.neighbors(node))
+        # Get rotational information
+        for node in graph.nodes():
+            neighbors = list(graph.neighbors(node))
 
             # zip neighbors with associated coordinates for sorting
-            neighbor_zip = zip(neighbors, [points[n] for n in neighbors])
+            neighbor_zip = list(zip(neighbors, [points[n] for n in neighbors]))
             
             anticlockwise, clockwise = False, True
 
             # Sort
-            sorted_zip = sorted(neighbor_zip, key = lambda pair: theta(pair[1], points[node]), reverse = anticlockwise)
+            sorted_zip = sorted(neighbor_zip, key=lambda pair: theta(pair[1], points[node]), reverse=anticlockwise)
 
             # Extract sorted edges
-            self.sorted_edges[node] = [(node, n) for (n, _ ) in sorted_zip]
+            self.sorted_edges[node] = [(node, n) for (n, _) in sorted_zip]
 
             # Not needed
-            self.sorted_neighbors[node] = [n for (n, _ ) in sorted_zip]
+            self.sorted_neighbors[node] = [n for (n, _) in sorted_zip]
 
             # Not 100% sure why e1, e2 need to be flipped to make things work
-            self.sorted_darts[node] = [self.edge2dart[(e2, e1)] for e1,e2 in self.sorted_edges[node]]
+            self.sorted_darts[node] = [self.edge2dart[(e2, e1)] for (e1, e2) in self.sorted_edges[node]]
 
-        self.G = Graph
+        self.G = graph
         self.points = points
-
-        self.darts = list(range(2*Graph.size()))
-        
+        self.darts = list(range(2*graph.size()))
 
     def sigma(self, dart):
         # Get node
@@ -70,8 +69,7 @@ class CMap :
         size = len(list(self.G.neighbors(node)))
 
         # Get next dart, wrap-around if out of range
-        return  self.sorted_darts[node][(index+1) % size]
-
+        return self.sorted_darts[node][(index+1) % size]
 
     def alpha(self, dart):
         # swap edge nodes, for other complement dart
@@ -83,30 +81,30 @@ class CMap :
 
     def plot(self):
         nx.draw_networkx_labels(self.G, self.points)
-        nx.draw_networkx_edge_labels(self.G, self.points, self.edge2dart, label_pos = 0.15)
+        nx.draw_networkx_edge_labels(self.G, self.points, self.edge2dart, label_pos=0.15)
         nx.draw(self.G, self.points)
         plt.show()
 
     def boundary_cycles(self):
         """
-            This function returns a list of the boundary cycles by iterating on phi(x).
-            The boundary cycles are given in terms of darts
-            Boundary Cycles are guarenteed to be disjoint
-            A cycle will have no repeated darts.
+            This function returns a list of the boundary cycles
+            by iterating on phi(x).
+            The boundary cycles are given in terms of darts.
+            Boundary Cycles form a partition of the darts.
         """
         output = []
         all_darts = self.darts.copy()
         
-        while(all_darts != []):
+        while all_darts:
             # set root
             cycle = [all_darts.pop()]
 
             # get next in cycle
             next_dart = self.phi(cycle[0])
 
-            while(next_dart != cycle[0]):
+            while next_dart != cycle[0]:
 
-                # remove dart, since dijoint
+                # remove dart, since disjoint
                 all_darts.remove(next_dart)
 
                 # add to cycle
@@ -115,7 +113,7 @@ class CMap :
                 # get next dart
                 next_dart = self.phi(next_dart)
 
-            # cycle finised when next_dart is root   
+            # cycle finished when next_dart is root
             output.append(cycle)
 
         return output
@@ -136,6 +134,11 @@ def boundary_cycle_graphs(cmap):
     return bcycles
 
 
+def is_hole(graph, alpha_shape):
+    # A hole cannot contain the boundary, or be a simplex
+    return not (set(alpha_shape).issubset(set(graph.nodes())) or graph.order() == 3)
+
+
 if __name__=="__main__":
     G = nx.house_x_graph()
     G.remove_edge(0, 1)
@@ -143,27 +146,24 @@ if __name__=="__main__":
     G.remove_edge(1, 3)
     G.add_edge(1, 0)
     G.add_edge(4, 0)
-    #G = nx.gnm_random_graph(10, 15)
-    points = [(cos(theta), sin(theta)) for theta in [2*pi*n/G.order() for n in range(G.order())]]
+    # G = nx.gnm_random_graph(10, 15)
+    mypoints = [(cos(theta), sin(theta)) for theta in [2*pi*n/G.order() for n in range(G.order())]]
 
-    cmap = CMap(G, points)
+    c_map = CMap(G, mypoints)
 
-    print(cmap.boundary_cycles())
+    print(c_map.boundary_cycles())
 
-    simplices = boundary_cycle_nodes(cmap)
+    simplices = boundary_cycle_graphs(c_map)
 
     boundary = [1, 2, 3, 4, 0]
 
     print([set(boundary) == set(G.nodes()) for G in simplices])
 
-    for i, simplex in enumerate(simplices, start = 1):
+    for i, simplex in enumerate(simplices, start=1):
         plot_num = str(2)+str(len(simplices)//2)+str(i)
         plt.subplot(int(plot_num))
-        nx.draw_networkx_nodes(G, points, node_color="b")
-        nx.draw_networkx_labels(G, points)
-        nx.draw(simplex, points,node_color="r")
+        nx.draw_networkx_nodes(G, dict(enumerate(mypoints)), node_color="b")
+        nx.draw_networkx_labels(G, dict(enumerate(mypoints)))
+        nx.draw(simplex, mypoints, node_color="r")
 
     plt.show()
-
-    
-    
