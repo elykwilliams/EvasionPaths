1405aefb5fb025c6c056389e458d263386de9d0c
kyle <williams.kyle@gmx.com>
2020-02-06 21:59:21 -0600

    Fix boundary reflection
    Currently implements virtual boundary of width dx

--- brownian_motion.py
+++ brownian_motion.py
@@ -17,25 +17,26 @@ class BrownianMotion:
     def generate_points(self, n_interior_pts):
         interior_pts = []
         for _ in range(n_interior_pts):
-            rand_x = np.random.uniform(self.boundary.x_min + self.radius/2, self.boundary.x_max - self.radius/2)
-            rand_y = np.random.uniform(self.boundary.y_min + self.radius/2, self.boundary.y_max - self.radius/2)
+            rand_x = np.random.uniform(self.boundary.x_min + self.radius, self.boundary.x_max - self.radius)
+            rand_y = np.random.uniform(self.boundary.y_min + self.radius, self.boundary.y_max - self.radius)
             interior_pts.append((rand_x, rand_y))
         return self.boundary.points + interior_pts
 
     def update_points(self, old_points):
+        dx = self.radius
+
         interior_pts = old_points[len(self.boundary):]
 
         interior_pts = [(x + self.epsilon(), y + self.epsilon()) for (x, y) in interior_pts]
 
-        # Boundary cases as implemented by Deepjoyti
         for n, (x, y) in enumerate(interior_pts):
-            if x >= 1:
-                interior_pts[n] = (2 - x - self.epsilon(), y)
-            if x <= 0:
-                interior_pts[n] = (-x + self.epsilon(), y)
-            if y >= 1:
-                interior_pts[n] = (x, 2 - y - self.epsilon())
-            if y <= 0:
-                interior_pts[n] = (x, -y + self.epsilon())
+            if x >= self.boundary.x_max - dx:
+                interior_pts[n] = (self.boundary.x_max - dx - 2*abs(self.epsilon()), y)
+            if x <= self.boundary.x_min + dx:
+                interior_pts[n] = (self.boundary.x_min + dx + 2*abs(self.epsilon()), y)
+            if y >= self.boundary.y_max - dx:
+                interior_pts[n] = (x, self.boundary.y_max - dx - 2*abs(self.epsilon()))
+            if y <= self.boundary.y_min + dx:
+                interior_pts[n] = (x, self.boundary.y_min + dx + 2*abs(self.epsilon()))
 
         return self.boundary.points + interior_pts
