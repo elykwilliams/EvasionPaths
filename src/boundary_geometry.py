fe133151644becc419ece2846c48ec1219752285
kyle <williams.kyle@gmx.com>
2020-05-03 14:25:21 -0500

    Refactor virtual boundary

--- src/boundary_geometry.py
+++ src/boundary_geometry.py
@@ -1,11 +1,10 @@
-# Kyle Williams 2/25/20
+# Kyle Williams 6/3/20
 
 import numpy as np
 
 
 class RectangularDomain:
-    """This class defines the boundary and stores data related to the boundary geometry. It does not store the boundary
-        points, simply generates and returns them """
+    """This class defines the boundary and stores data related to the boundary geometry."""
 
     def __init__(self, spacing: float = 0.2,  # default of 0.2, with unit square
                  x_min: float = 0, x_max: float = 1,
@@ -13,6 +12,10 @@ class RectangularDomain:
         self.x_max, self.y_max = x_max, y_max
         self.x_min, self.y_min = x_min, y_min
         self.points = []
+
+        self.dx = spacing * np.sin(np.pi / 6)  # virtual boundary width
+        self.vx_min, self.vx_max = self.x_min - self.dx, self.x_max + self.dx
+        self.vy_min, self.vy_max = self.y_min - self.dx, self.y_max + self.dx
 
         self.generate(spacing)
 
@@ -32,19 +35,14 @@ class RectangularDomain:
 
     def generate(self, spacing: float) -> list:
         """ Generate a list of points (represent by tuples)
-            Add corners explicitly in case boundary width is not divisible by radius
             Spacing should be *at most* 2*sensing_radius when used.
+            Points should be added in cyclic order.
         """
-        dx = spacing * np.sin(np.pi / 6)  # virtual boundary width
-        corners = [(self.x_min-dx, self.y_min-dx), (self.x_min-dx, self.y_max+dx),
-                   (self.x_max+dx, -dx), (self.x_max+dx, self.y_max+dx)]
 
-        self.points = corners
-
-        self.points.extend([(x, self.y_min-dx) for x in np.arange(self.x_min - dx, self.x_max+dx, spacing)])  # bottom
-        self.points.extend([(self.x_min-dx, y) for y in np.arange(self.y_min - dx, self.y_max+dx, spacing)])  # left
-        self.points.extend([(self.x_max - x, self.y_max+dx) for x in np.arange(self.x_min - dx, self.x_max+dx, spacing)])  # top
-        self.points.extend([(self.x_max+dx, self.y_max - y) for y in np.arange(self.y_min - dx, self.y_max+dx, spacing)])  # right
+        self.points.extend([(x, self.vy_min) for x in np.arange(self.vx_min, self.vx_max, spacing)])  # bottom
+        self.points.extend([(self.vx_max, y) for y in np.arange(self.vy_min, self.vx_max, spacing)])  # right
+        self.points.extend([(self.x_max - x, self.vy_max) for x in np.arange(self.vx_min, self.vx_max, spacing)])  # top
+        self.points.extend([(self.vx_min, self.y_max - y) for y in np.arange(self.vy_min, self.vy_max, spacing)])  # left
 
         return self.points
 
@@ -85,7 +83,10 @@ class CircularDomain:
 if __name__=="__main__":
     import matplotlib.pyplot as plt
 
-    unit_square = RectangularDomain(0.15)
+    sensing_radius = 0.15
+
+    # Draw vi
+    unit_square = RectangularDomain(sensing_radius)
     x_pts = [x for x, _ in unit_square.points]
     y_pts = [y for _, y in unit_square.points]
     plt.plot(x_pts, y_pts, "*")
@@ -96,6 +97,12 @@ if __name__=="__main__":
     plt.plot(x_us, y_us)
 
     ax = plt.gca()
-    ax.axis("equal")
 
+    for point in unit_square.points:
+        ax.add_artist(plt.Circle(point, sensing_radius, color='b', alpha=0.1))
+
+    ax.axis('equal')
+    ax.set(xlim=(unit_square.vx_min - 1.1*sensing_radius, unit_square.vx_max + 1.1*sensing_radius),
+           ylim=(unit_square.vy_min - 1.1*sensing_radius, unit_square.vy_max + 1.1*sensing_radius))
+    ax.set_aspect('equal', 'box')
     plt.show()
