4b3709833bcf58a442b3160e91b632cf0029b125
kyle <williams.kyle@gmx.com>
2020-05-07 01:48:08 -0500

    Implement StageChange.is_valid()

--- src/cycle_labelling.py
+++ src/cycle_labelling.py
@@ -1,125 +1,110 @@
 from states import *
 from combinatorial_map import simplex2cycle
 
-case_name = {
-    (0, 0, 0, 0, 0, 0): "",
-    (1, 0, 0, 0, 2, 1): "Add 1-Simplex",
-    (1, 0, 0, 0, 1, 0): "Add 1-Simplex",
-    (0, 1, 0, 0, 1, 2): "Remove 1-Simplex",
-    (0, 1, 0, 0, 0, 1): "Remove 1-Simplex",
-    (0, 0, 1, 0, 0, 0): "Add 2-Simplex",
-    (0, 0, 0, 1, 0, 0): "Remove 2-Simplex",
-    (1, 0, 1, 0, 2, 1): "Add 1-Simplex and 2-Simplex",
-    (0, 1, 0, 1, 1, 2): "Remove 1-Simplex and 2-Simplex",
-    (1, 1, 2, 2, 2, 2): "Delauney Flip",
-    (0, 1, 0, 0, 2, 1): "Disconnect",
-    (0, 1, 0, 0, 1, 1): "Disconnect",
-    (1, 0, 0, 0, 1, 2): "Reconnect",
-    (1, 0, 0, 0, 1, 1): "Reconnect"
-}
 
 class CycleLabelling:
     # True = possible intruder
     # False = no intruder
     def __init__(self, state):
-        self.cell_label = dict()
+        self.cycle_label = dict()
         for cycle in state.boundary_cycles:
-            self.cell_label[cycle] = True
+            self.cycle_label[cycle] = True
 
-        simplex_cycles = [simplex2cycle(simplex, state.boundary_cycles) for simplex in state.simplices2]
+        simplex_cycles \
+            = [simplex2cycle(simplex, state.boundary_cycles) for simplex in state.simplices2]
         for simplex in simplex_cycles:
             if state.is_connected(simplex):
-                self.cell_label[simplex] = False
+                self.cycle_label[simplex] = False
             else:
-                del self.cell_label[simplex]
+                del self.cycle_label[simplex]
 
     def __getitem__(self, item):
-        return self.cell_label[item]
+        return self.cycle_label[item]
 
     def __setitem__(self, key, value):
-        self.cell_label[key] = value
+        self.cycle_label[key] = value
 
     def __str__(self):
         res = ""
-        for key in self.cell_label.keys():
-            res += str(key)+": " + str(self.cell_label[key]) + "\n"
+        for key in self.cycle_label:
+            res += str(key) + ": " + str(self.cycle_label[key]) + "\n"
         return res
 
     def __contains__(self, item):
-        return item in self.cell_label.keys()
+        return item in self.cycle_label
 
     def delete_cycle(self, cycle):
-        del self.cell_label[cycle]
+        del self.cycle_label[cycle]
 
     def has_intruder(self):
-        return any(self.cell_label.values())
+        return any(self.cycle_label.values())
 
     def delaunay_flip(self, cycles_removed, cycles_added):
-        # Add new boundary cycles
         for cycle in cycles_added:
-            self.cell_label[cycle] = False
-        # Remove old boundary cycles
+            self.cycle_label[cycle] = False
         for cycle in cycles_removed:
-            del self.cell_label[cycle]
+            del self.cycle_label[cycle]
 
     def remove_one_two_simplex(self, cycles_removed, new_cycle):
         # Add new boundary cycles to dictionary, they retain the same label as the old cycle
-        self.cell_label[new_cycle] = any([self.cell_label[s] for s in cycles_removed])
-        # Remove old boundary cycle from dictionary
+        self.cycle_label[new_cycle] = any([self.cycle_label[s] for s in cycles_removed])
         for cycle in cycles_removed:
-            del self.cell_label[cycle]
+            del self.cycle_label[cycle]
 
     def add_one_two_simplex(self, old_cycle, cycles_added, added_simplex):
         # Add new boundary cycles to dictionary, they retain the same label as the old cycle
         for cycle in cycles_added:
-            self.cell_label[cycle] = self.cell_label[old_cycle]
-        self.cell_label[added_simplex] = False
+            self.cycle_label[cycle] = self.cycle_label[old_cycle]
+        self.cycle_label[added_simplex] = False
         # Remove old boundary cycle from dictionary
-        del self.cell_label[old_cycle]
+        del self.cycle_label[old_cycle]
 
     def add_twosimplex(self, new_cycle):
         # Update existing boundary cycle
-        self.cell_label[new_cycle] = False
+        self.cycle_label[new_cycle] = False
 
     def remove_onesimplex(self, cycles_removed, new_cycle):
         # Add new boundary cycles to dictionary, they retain the same label as the old cycle
-        self.cell_label[new_cycle] = any([self.cell_label[s] for s in cycles_removed])
+        self.cycle_label[new_cycle] = any([self.cycle_label[s] for s in cycles_removed])
         # Remove old boundary cycle from dictionary
         for cycle in cycles_removed:
-            del self.cell_label[cycle]
+            del self.cycle_label[cycle]
 
     def add_onesimplex(self, old_cycle, cycles_added):
         # Add new boundary cycles to dictionary, they retain the same label as the old cycle
         for cycle in cycles_added:
-            self.cell_label[cycle] = self.cell_label[old_cycle]
+            self.cycle_label[cycle] = self.cycle_label[old_cycle]
         # Remove old boundary cycle from dictionary
-        del self.cell_label[old_cycle]
+        del self.cycle_label[old_cycle]
 
-    def update_labelling(self, state_change):
+    def update(self, state_change):
+        if not state_change.is_valid():
+            raise InvalidStateChange(state_change)
+
         # No Change
         if state_change.case == (0, 0, 0, 0, 0, 0):
-            return ""
+            return
 
         # Add Edge
         elif state_change.case == (1, 0, 0, 0, 2, 1):
-            old_cycle = state_change.cycles_removed.pop()
-            if old_cycle not in self.cell_label:
-                return ""
+            old_cycle = state_change.cycles_removed[0]
+            if old_cycle not in self.cycle_label:
+                return
             self.add_onesimplex(old_cycle, state_change.cycles_added)
 
         # Remove Edge
         elif state_change.case == (0, 1, 0, 0, 1, 2):
-            new_cycle = state_change.cycles_added.pop()
-            if any([cell not in self.cell_label for cell in state_change.cycles_removed]):
-                return ""
+            if any([cell not in self.cycle_label for cell in state_change.cycles_removed]):
+                return
 
+            new_cycle = state_change.cycles_added[0]
             self.remove_onesimplex(state_change.cycles_removed, new_cycle)
 
         # Add Simplex
         elif state_change.case == (0, 0, 1, 0, 0, 0):
-            simplex = state_change.simplices_added.pop()
+            simplex = state_change.simplices_added[0]
             new_cycle = simplex2cycle(simplex, state_change.new_state.boundary_cycles)
-            if new_cycle not in self.cell_label:
+            if new_cycle not in self.cycle_label:
                 return ""
 
             self.add_twosimplex(new_cycle)
@@ -127,76 +112,57 @@ class CycleLabelling:
         # Remove Simplex
         elif state_change.case == (0, 0, 0, 1, 0, 0):
             # No label change needed
-            pass
+            return
 
         # Edge and Simplex Added
         elif state_change.case == (1, 0, 1, 0, 2, 1):
-            old_cycle = state_change.cycles_removed.pop()
-            simplex = state_change.simplices_added.pop()
+            old_cycle = state_change.cycles_removed[0]
+            simplex = state_change.simplices_added[0]
             added_simplex = simplex2cycle(simplex, state_change.new_state.boundary_cycles)
 
-            if not set(state_change.edges_added.pop()).issubset(set(simplex)):
-                raise InvalidStateChange(state_change)
-
-            if old_cycle not in self.cell_label:
-                return ""
+            if old_cycle not in self.cycle_label:
+                return
 
             self.add_one_two_simplex(old_cycle, state_change.cycles_added, added_simplex)
 
         # Edge and Simplex Removed
         elif state_change.case == (0, 1, 0, 1, 1, 2):
-            simplex = state_change.simplices_removed.pop()
-            new_cycle = state_change.cycles_added.pop()
+            new_cycle = state_change.cycles_added[0]
 
-            if not set(state_change.edges_removed.pop()).issubset(set(simplex)):
-                raise InvalidStateChange(state_change)
-
-            if any([cell not in self.cell_label for cell in state_change.cycles_removed]):
+            if any([cell not in self.cycle_label for cell in state_change.cycles_removed]):
                 return
 
             self.remove_one_two_simplex(state_change.cycles_removed, new_cycle)
 
         # Delunay Flip
         elif state_change.case == (1, 1, 2, 2, 2, 2):
-            oldedge = state_change.edges_removed.pop()
-            newedge = state_change.edges_added.pop()
 
-            if not all([cycle in self.cell_label for cycle in state_change.cycles_removed]):
-                return ""
-
-            # Check that edges correspond to correct boundary cycles
-            if not all([set(oldedge).issubset(set(s)) for s in state_change.simplices_removed]):
-                raise InvalidStateChange(state_change)
-            elif not all([set(newedge).issubset(set(s)) for s in state_change.simplices_added]):
-                raise InvalidStateChange(state_change)
-            elif not all([set(s).issubset(set(oldedge).union(set(newedge))) for s in state_change.simplices_removed]):
-                raise InvalidStateChange(state_change)
-            elif not all([set(s).issubset(set(oldedge).union(set(newedge))) for s in state_change.simplices_added]):
-                raise InvalidStateChange(state_change)
+            if not all([cycle in self.cycle_label for cycle in state_change.cycles_removed]):
+                return
 
             self.delaunay_flip(state_change.cycles_removed, state_change.cycles_added)
 
         # Disconnect
         elif state_change.case == (0, 1, 0, 0, 2, 1) or state_change.case == (0, 1, 0, 0, 1, 1):
-            old_cycle = state_change.cycles_removed.pop()
-            if old_cycle not in self.cell_label:
-                return ""
+            old_cycle = state_change.cycles_removed[0]
+            if old_cycle not in self.cycle_label:
+                return
 
-            enclosing_cycle = state_change.cycles_added.pop()
+            enclosing_cycle = state_change.cycles_added[0]
             if not state_change.new_state.is_connected(enclosing_cycle) and len(state_change.cycles_added) != 0:
-                enclosing_cycle = state_change.cycles_added.pop()
+                enclosing_cycle = state_change.cycles_added[1]
 
             # Find labelled cycles that have become disconnected,
             # this works because all other disconnected cycles have been forgotten
             disconnected_cycles = []
             for cycle in state_change.new_state.boundary_cycles:
-                if not state_change.new_state.is_connected(cycle) and cycle in self.cell_label:
+                if not state_change.new_state.is_connected(cycle) and cycle in self.cycle_label:
                     disconnected_cycles.append(cycle)
 
             # Enclosing cycle will be clear if the old enclosing cycle and all disconnected cycles are clear
-            intruder_subcycle = any([self.cell_label[cycle] for cycle in disconnected_cycles])
+            intruder_subcycle = any([self.cycle_label[cycle] for cycle in disconnected_cycles])
 
-            self.cell_label[enclosing_cycle] = intruder_subcycle or self.cell_label[old_cycle]
+            self.cycle_label[enclosing_cycle] = intruder_subcycle or self.cycle_label[old_cycle]
 
             # Forget disconnected cycles
             for cycle in disconnected_cycles:
@@ -206,29 +172,29 @@ class CycleLabelling:
 
         # Reconnect
         elif state_change.case == (1, 0, 0, 0, 1, 2) or state_change.case == (1, 0, 0, 0, 1, 1):
-            enclosing_cycle = state_change.cycles_removed.pop()
-            if enclosing_cycle not in self.cell_label and len(state_change.cycles_removed) != 0:
-                enclosing_cycle = state_change.cycles_removed.pop()
+            enclosing_cycle = state_change.cycles_removed[0]
+            if enclosing_cycle not in self.cycle_label and len(state_change.cycles_removed) != 0:
+                enclosing_cycle = state_change.cycles_removed[1]
 
-            if enclosing_cycle not in self.cell_label:
-                return ""
+            if enclosing_cycle not in self.cycle_label:
+                return
 
             # Find labelled cycles that have just become connected,
             # These will be all boundary cycles that are connected and have no label
-            new_cycle = state_change.cycles_added.pop()
+            new_cycle = state_change.cycles_added[0]
 
             # Newly connected cycles have label to match old enclosing cycle
-            self.cell_label[new_cycle] = self.cell_label[enclosing_cycle]
+            self.cycle_label[new_cycle] = self.cycle_label[enclosing_cycle]
 
             # Add back any forgotten cycle
             for cycle in state_change.new_state.boundary_cycles:
-                if state_change.new_state.is_connected(cycle) and cycle not in self.cell_label:
-                    self.cell_label[cycle] = self.cell_label[enclosing_cycle]
+                if state_change.new_state.is_connected(cycle) and cycle not in self.cycle_label:
+                    self.cycle_label[cycle] = self.cycle_label[enclosing_cycle]
 
             # Reset all connected 2-simplices to have no intruder
             for simplex in state_change.new_state.simplices2:
                 cycle = simplex2cycle(simplex, state_change.new_state.boundary_cycles)
-                if cycle not in self.cell_label:
+                if cycle not in self.cycle_label:
                     continue
                 self.add_twosimplex(cycle)
 
@@ -237,11 +203,8 @@ class CycleLabelling:
 
         # two isolated points connecting
         elif state_change.case == (1, 0, 0, 0, 1, 0):
-            return ""
+            return
+
         # two points becoming isolated
         elif state_change.case == (0, 1, 0, 0, 0, 1):
-            return ""
-        else:
-            raise InvalidStateChange(state_change)
-
-        return case_name[state_change.case] + ", "
+            return
--- src/states.py
+++ src/states.py
@@ -4,7 +4,29 @@ import networkx as nx
 
 
 def set_difference(list1, list2):
-    return set(list1).difference(set(list2))
+    return list(set(list1).difference(set(list2)))
+
+
+def is_subset(list1, list2):
+    return set(list1).issubset(set(list2))
+
+
+case2name = {
+    (0, 0, 0, 0, 0, 0): "",
+    (1, 0, 0, 0, 2, 1): "Add 1-Simplex",
+    (1, 0, 0, 0, 1, 0): "Add 1-Simplex",
+    (0, 1, 0, 0, 1, 2): "Remove 1-Simplex",
+    (0, 1, 0, 0, 0, 1): "Remove 1-Simplex",
+    (0, 0, 1, 0, 0, 0): "Add 2-Simplex",
+    (0, 0, 0, 1, 0, 0): "Remove 2-Simplex",
+    (1, 0, 1, 0, 2, 1): "Add 1-Simplex and 2-Simplex",
+    (0, 1, 0, 1, 1, 2): "Remove 1-Simplex and 2-Simplex",
+    (1, 1, 2, 2, 2, 2): "Delauney Flip",
+    (0, 1, 0, 0, 2, 1): "Disconnect",
+    (0, 1, 0, 0, 1, 1): "Disconnect",
+    (1, 0, 0, 0, 1, 2): "Reconnect",
+    (1, 0, 0, 0, 1, 1): "Reconnect"
+}
 
 
 class State(object):
@@ -23,10 +45,10 @@ class State(object):
         self.boundary_cycles = CMap(graph, points).get_boundary_cycles()
         self.boundary_cycles.remove(boundary.alpha_cycle)
 
-        self.connected_nodes = nx.node_connected_component(graph, 0)
+        self._connected_nodes = nx.node_connected_component(graph, 0)
 
     def is_connected(self, cycle):
-        return set(cycle2nodes(cycle)).intersection(self.connected_nodes) != set()
+        return set(cycle2nodes(cycle)).intersection(self._connected_nodes) != set()
 
 
 class StateChange(object):
@@ -43,6 +65,40 @@ class StateChange(object):
 
         self.case = (len(self.edges_added), len(self.edges_removed), len(self.simplices_added),
                      len(self.simplices_removed), len(self.cycles_added), len(self.cycles_removed))
+
+    def is_valid(self):
+        if self.case not in case2name.keys():
+            return False
+        elif self.case == (1, 0, 1, 0, 2, 1):
+            simplex = self.simplices_added[0]
+            edge = self.edges_added[0]
+            if not is_subset(edge, simplex):
+                return False
+        elif self.case == (0, 1, 0, 1, 1, 2):
+            simplex = self.simplices_removed[0]
+            edge = self.edges_removed[0]
+            if not is_subset(edge, simplex):
+                return False
+        elif self.case == (1, 1, 2, 2, 2, 2):
+            old_edge = self.edges_removed[0]
+            new_edge = self.edges_added[0]
+            if not all([is_subset(old_edge, s) for s in self.simplices_removed]):
+                return False
+            elif not all([is_subset(new_edge, s) for s in self.simplices_added]):
+                return False
+
+            nodes = set(old_edge).union(set(new_edge))
+            if not all([is_subset(s, nodes) for s in self.simplices_removed]):
+                return False
+            elif not all([is_subset(s, nodes) for s in self.simplices_added]):
+                return False
+        return True
+
+    def get_name(self):
+        if self.is_valid():
+            return case2name[self.case]
+        else:
+            return "Invalid Case"
 
     def __str__(self):
         return "State Change:\n" \
@@ -70,4 +126,13 @@ class InvalidStateChange(Exception):
 
     def __str__(self):
         return "Invalid State Change \n\n" \
+               + str(self.state_change)
+
+
+class BadStateChange(Exception):
+    def __init__(self, state_change):
+        self.state_change = state_change
+
+    def __str__(self):
+        return "Invalid State Change not caught in is_valid \n\n" \
                + str(self.state_change)
