72db93a99f32000757105d5f0b239627ff478c47
kyle <williams.kyle@gmx.com>
2020-05-07 20:18:43 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='EvasionPaths',
+    version='0.0.2',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='https://bitbucket.org/KyleWill/evasion-paths',
+    license='BSD-3',
+    author='Kyle Williams',
+    author_email='kylew@math.uh.edu',
+    description='Tracking of evasion paths in mobile sensor networks'
+)
