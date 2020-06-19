5a5f4a45644331f018d5ba482d77bbb340e4a8c5
kyle <williams.kyle@gmx.com>
2020-06-19 02:41:09 -0500

    Update version
    The updates in how reflection is done will have breaking changes
    for any externally implemented motion models

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EvasionPaths',
-    version='0.0.2',
+    version='0.1.0',
     packages=[''],
     package_dir={'': 'src'},
     url='https://bitbucket.org/KyleWill/evasion-paths',
