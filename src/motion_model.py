7e72e40c40be831e636998b12dcac5e2ac2c9861
kyle <williams.kyle@gmx.com>
2020-06-24 17:18:04 -0500

    Add D'Orsogna Model

--- src/motion_model.py
+++ src/motion_model.py
@@ -137,6 +137,104 @@ class Viscek(BilliardMotion):
 
         for i, index_list in enumerate(indices):
             if index_list:
-                self.vel_angle[i] = (mean([self.vel_angle[j] for j in index_list])+self.eta()) % (2 * pi)
+                self.vel_angle[i] = (mean([self.vel_angle[j] for j in index_list]) + self.eta()) % (2 * pi)
 
         return new_points
+
+
+class DorsognaModel(MotionModel):
+    def __init__(self, dt, boundary, max_vel, n_int_sensors, sensing_radius, eta_scale_factor, DO_coeff):
+        super().__init__(dt, boundary)
+        self.velocities = np.random.uniform(-max_vel, max_vel, (n_int_sensors, 2))
+        self.n_sensors = n_int_sensors
+        self.boundary = boundary
+        self.sensing_radius = sensing_radius
+        self.eta = eta_scale_factor * sensing_radius
+        self.DO_coeff = DO_coeff
+
+    def update_point(self, pt: tuple, index: int) -> tuple:
+        #Not used
+        return pt
+
+    def reflect(self, old_pt, new_pt, index) -> tuple:
+        v = self.velocities[index]
+        norm_v = sqrt(v[0]**2 + v[1]**2)
+        theta = self.boundary.reflect_velocity(old_pt, new_pt)
+        pt = self.boundary.reflect_point(old_pt, new_pt)
+        self.velocities[index] = (norm_v*cos(theta), norm_v*sin(theta))
+        return pt
+
+
+    def gradient(self, xs, ys):
+        gradUx, gradUy = [0] * self.n_sensors, [0] * self.n_sensors
+
+        for i in range(0, self.n_sensors):
+            pairwise_dist = [0] * self.n_sensors
+            attract_term, repel_term = [0] * self.n_sensors, [0] * self.n_sensors
+
+            for j in range(0, self.n_sensors):
+                if norm((xs[i] - xs[j], ys[i] - ys[j])) > 2 * self.sensing_radius:
+                    pairwise_dist[j] = 0.0
+                else:
+                    pairwise_dist[j] = norm((xs[i] - xs[j], ys[i] - ys[j]))
+
+            for j in range(0, self.n_sensors):
+                if pairwise_dist[j] == 0.0:
+                    attract_term[j] = 0
+                    repel_term[j] = 0
+                else:
+                    attract_term[j] = self.DO_coeff[0]*np.exp(-pairwise_dist[j]/self.DO_coeff[1])/(self.DO_coeff[1]*pairwise_dist[j])
+                    repel_term[j] = self.DO_coeff[2]*np.exp(-pairwise_dist[j]/self.DO_coeff[3])/(self.DO_coeff[3] * pairwise_dist[j])
+
+            gradUx[i] = sum(((xs[i]-xs[j]) * attract_term[j]) - ((xs[i]-xs[j]) * repel_term[j])
+                               for j in range(0, self.n_sensors) if pairwise_dist[j] != 0.0)
+            gradUy[i] = sum(((ys[i]-ys[j]) * attract_term[j]) - ((ys[i]-ys[j]) * repel_term[j])
+                               for j in range(0, self.n_sensors) if pairwise_dist[j] != 0.0)
+
+        return array(gradUx), array(gradUy)
+
+
+    def time_derivative(self, _, state):
+        # ode solver gives us np array in the form [xvals | yvals | vxvals | vyvals]
+        # split into individual np array
+        split_state = [state[i:i + self.n_sensors] for i in range(0, len(state), self.n_sensors)]
+        gradU = self.gradient(split_state[0], split_state[1])
+
+        # Need to compute time derivative of each,
+        # I just have d(x, y)/dt = (vx, vy), d(vx, vy)/dt = (1, -1)
+        dxdt = split_state[2]
+        dydt = split_state[3]
+        dvxdt = (1.5 - (0.5 * norm((dxdt, dydt))**2)) * dxdt - gradU[0]
+        dvydt = (1.5 - (0.5 * norm((dxdt, dydt))**2)) * dydt - gradU[1]
+        return np.concatenate([dxdt, dydt, dvxdt, dvydt])
+
+
+    def update_points(self, old_points, dt) -> list:
+        self.dt = dt
+        # Remove boundary points, and put into form ode solver wants
+        xs = [old_points[i][0] for i in range(len(self.boundary), len(old_points))]
+        ys = [old_points[i][1] for i in range(len(self.boundary), len(old_points))]
+        vxs = [self.velocities[i][0] for i in range(self.n_sensors)]
+        vys = [self.velocities[i][1] for i in range(self.n_sensors)]
+        init_val = xs + ys + vxs + vys
+
+        # Solve with init_val as t=0, solve for values at t+dt
+        new_val = solve_ivp(self.time_derivative, [0, self.dt], init_val, t_eval=[self.dt], rtol=1e-8)
+
+        # Convert back from np array
+        new_val = new_val.y.tolist()
+
+        # split state back into position and velocity,
+        split_state = [[y[0] for y in new_val[i:i + self.n_sensors]]
+                       for i in range(0, len(new_val), self.n_sensors)]
+
+        # zip into list of tuples
+        self.velocities = list(zip(split_state[2], split_state[3]))
+
+        # Reflect any points outside boundary
+        points = list(zip(split_state[0], split_state[1]))
+        for n, pt in enumerate(points):
+            if not self.boundary.in_domain(pt):
+                points[n] = self.reflect(old_points[n+len(self.boundary)], pt, n)
+
+        return old_points[0:len(self.boundary)] + points
