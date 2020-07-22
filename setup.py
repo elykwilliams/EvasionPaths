elykwilliams/EvasionPaths

commit b60ffb729ed5db227cbfb8e4af8682151746af8f

Fixes Makefile issues with conda
Also adds option to make update so that the environment doesn't
need to be reinstalled while pulling a simple update.

setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name='EvasionPaths',
     version='0.1.1',
-    packages=[''],
     package_dir={'': 'src'},
     url='https://bitbucket.org/KyleWill/evasion-paths',
     license='BSD-3',

Makefile
@@ -1,5 +1,15 @@
+SHELL:=/bin/bash
+.ONESHELL:
 
 install:
 	conda env create --file environment.yml	--force
+	source ~/.bashrc
+	conda activate EvasionPaths-env
+	python3 setup.py install 
+
+update:
+	git pull
+	source ~/.bashrc
 	conda activate EvasionPaths-env
 	python3 setup.py install
+

commit a0eb0fa9c834b03f92637101a99ccabf087ef4f6

Fix problem with package install
while the setup appears to work, it does not actually copy the src
files properly resulting in examples being unable to find the
source modules.

This was due to an error introduced in b60ffb7

setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='EvasionPaths',
     version='0.1.1',
+    packages=[''],
     package_dir={'': 'src'},
     url='https://bitbucket.org/KyleWill/evasion-paths',
     license='BSD-3',