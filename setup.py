bb4b39631eda978f6be3bc2e710c9f2cfb032d8f
Kyle Williams <williams.kyle@gmx.com>
2021-05-10 00:28:58 -0500

    Branch for Paper Release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EvasionPaths',
-    version='0.1.1',
+    version='1.0.0',
     packages=[''],
     package_dir={'': 'src'},
     url='https://bitbucket.org/KyleWill/evasion-paths',
