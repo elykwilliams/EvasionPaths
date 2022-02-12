123b880f88b0a4c748f834703ff9103721907f6f
Kyle Williams <williams.kyle@gmx.com>
2022-02-12 15:17:19 -0600

    style: Simplifies time_stepping

--- src/time_stepping.py
+++ src/time_stepping.py
@@ -40,7 +40,10 @@ class EvasionPathSimulation:
     # exit if max time is set. Returns simulation time.
     def run(self) -> float:
         while self.cycle_label.has_intruder():
-            self.do_timestep()
+            try:
+                self.do_timestep()
+            except MaxRecursionDepthError:
+                raise  # do self_dump
             self.time += self.dt
             if 0 < self.Tend < self.time:
                 break
@@ -51,6 +54,8 @@ class EvasionPathSimulation:
     # If change is non-atomic, split time step in half and solve recursively.
     # Once an atomic change is found, update sensor network position, and update labelling.
     def do_timestep(self, level: int = 0) -> None:
+        if level == 25:
+            raise MaxRecursionDepthError()
 
         dt = self.dt * 2 ** -level
 
@@ -61,22 +66,18 @@ class EvasionPathSimulation:
 
             label_update = LabelUpdateFactory().get_update(state_change, self.cycle_label)
 
-            if label_update.is_atomic() and self.cycle_label.is_valid(label_update):
-                self.cycle_label.update(label_update)
-                self.sensor_network.update()
-                self.topology = state_change.new_topology
-            elif level + 1 == 25:
-                raise MaxRecursionDepth(state_change)
+            if label_update.is_atomic():
+                self.update(label_update, new_topology)
             else:
                 self.do_timestep(level=level + 1)
 
             if level == 0:
                 return
 
-    def update(self, state_change):
-        self.cycle_label.update(state_change)
+    def update(self, label_update, new_topology):
+        self.cycle_label.update(label_update)
         self.sensor_network.update()
-        self.topology = state_change.new_state
+        self.topology = new_topology
 
 
 ## Takes output from save_state() to initialize a simulation.
