9ccfdfda893b890f9c914015d90d8b12c3b38e43
Kyle W <williams.kyle@gma.com>
2023-06-10 22:59:35 -0500

    Clean up ReebGraph

--- src/reeb_graph.py
+++ src/reeb_graph.py
@@ -1,6 +1,5 @@
 import networkx as nx
 
-
 case_name = {
     (0, 0): "Trivial",
     (1, 0): "Birth",
@@ -11,72 +10,67 @@ case_name = {
 }
 
 
-def get_sym_diff(holes2: set, holes1: set):
-    """(added, removed)"""
-    return holes2.difference(holes1), holes1.difference(holes2)
+def get_sym_diff(set1: set, set2: set):
+    """Returns symmetric difference between two sets"""
+    return set1.difference(set2), set2.difference(set1)
 
 
 def get_case(added: set, removed: set) -> str:
+    """Returns the case name based on the size of the added and removed sets"""
     return case_name[(len(added), len(removed))]
 
 
 class ReebGraph:
+    """ReebGraph class to handle operations on graph"""
+
     def __init__(self, holes: dict):
         self.graph = nx.Graph()
         self.stack = dict()
         self.finalized = []
 
         # Initial update
-        for h in holes:
-            height = self.get_height(set())
-            event_id = self.insert_new_node(0, height, "Init")
-            self.stack[h] = (event_id, height)
+        for hole in holes:
+            height = self._get_height(set())
+            event_id = self._insert_new_node(0, height, "Init")
+            self.stack[hole] = (event_id, height)
 
-    def insert_new_node(self, time: float, height: float, name: str):
+    def _insert_new_node(self, time: float, height: float, name: str):
         event_id = len(self.graph)
         self.graph.add_node(event_id, name=name, pos=(time, height))
         return event_id
 
-    def insert_new_edge(self, oldId: int, newId: int, val: bool) -> None:
-        color = 'r' if val else 'g'
-        self.graph.add_edge(oldId, newId, color=color)
+    def _insert_new_edge(self, oldId: int, newId: int, val: bool) -> None:
+        self.graph.add_edge(oldId, newId, val=val)
 
-    def update(self, time, holes_dict2, holes_dict1):
-        # Determine event type
-        added, removed = get_sym_diff(set(holes_dict2.keys()), set(holes_dict1.keys()))
+    def update(self, time, new_holes_dict, old_holes_dict):
+        added, removed = get_sym_diff(set(new_holes_dict.keys()), set(old_holes_dict.keys()))
         name = get_case(added, removed)
+        height = self._get_height(removed)
+        node_id = self._insert_new_node(time, height, name)
 
-        # determine height
-        height = self.get_height(removed)
-
-        # insert event node
-        nodeId = self.insert_new_node(time, height, name)
-
-        # connect removed boundary cycles
-        for h in removed:
-            eventId, _ = self.stack[h]
-            self.insert_new_edge(eventId, nodeId, holes_dict1[h])
-            del self.stack[h]
+        # Connect removed boundary cycles
+        for hole in removed:
+            event_id, _ = self.stack[hole]
+            self._insert_new_edge(event_id, node_id, old_holes_dict[hole])
+            del self.stack[hole]
 
         # Push new boundary cycles onto the stack
         for hole in sorted(added):
-            self.stack[hole] = (nodeId, height)
+            self.stack[hole] = (node_id, height)
             height = len(self.stack)
 
-    def get_height(self, removed: set):
-        if removed:
-            return min(self.stack[hole][1] for hole in removed)
-        return len(self.stack)
+    def _get_height(self, removed: set):
+        return min(self.stack[hole][1] for hole in removed) if removed else len(self.stack)
 
     def finalize(self, time, holes):
         if self.finalized:
             return
 
-        for h in holes:
-            oldId, height = self.stack[h]
-            newId = self.insert_new_node(time, height, "final")
-            self.insert_new_edge(oldId, newId, holes[h])
-            self.finalized.append(newId)
+        for hole in holes:
+            old_id, height = self.stack[hole]
+            new_id = self._insert_new_node(time, height, "final")
+            self._insert_new_edge(old_id, new_id, holes[hole])
+            self.finalized.append(new_id)
 
     def resume(self):
         self.graph.remove_nodes_from(self.finalized)
