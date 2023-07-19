1ea852ddcef018dd3dd74a9ba0d7844e7da018ab
Kyle W <williams.kyle@gma.com>
2023-07-19 11:13:15 -0500

    Update Copyright

--- src/cycle_labelling.py
+++ src/cycle_labelling.py
@@ -1,3 +1,10 @@
+# ******************************************************************************
+#  Copyright (c) 2023, Kyle Williams - All Rights Reserved.
+#  You may use, distribute and modify this code under the terms of the BSD-3
+#  license. You should have received a copy of the BSD-3 license with this file.
+#  If not, visit: https://opensource.org/licenses/BSD-3-Clause
+# ******************************************************************************
+
 from reeb_graph import ReebGraph
 from state_change import StateChange
 from topology import Topology
--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -1,3 +1,10 @@
+# ******************************************************************************
+#  Copyright (c) 2023, Kyle Williams - All Rights Reserved.
+#  You may use, distribute and modify this code under the terms of the BSD-3
+#  license. You should have received a copy of the BSD-3 license with this file.
+#  If not, visit: https://opensource.org/licenses/BSD-3-Clause
+# ******************************************************************************
+
 from unittest import mock
 
 import numpy as np
