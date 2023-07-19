7fced11c01577a0a5bd69d6e00d297c44c507994
Kyle W <williams.kyle@gma.com>
2023-07-19 11:13:15 -0500

    Update Copyright

--- src/sensor_network.py
+++ src/sensor_network.py
@@ -1,10 +1,9 @@
-# ************************************************************
-# Copyright (c) 2021, Kyle Williams - All Rights Reserved.
-# You may use, distribute and modify this code under the
-# terms of the BSD-3 license. You should have received a copy
-# of the BSD-3 license with this file.
-# If not, visit: https://opensource.org/licenses/BSD-3-Clause
-# ************************************************************
+# ******************************************************************************
+#  Copyright (c) 2023, Kyle Williams - All Rights Reserved.
+#  You may use, distribute and modify this code under the terms of the BSD-3
+#  license. You should have received a copy of the BSD-3 license with this file.
+#  If not, visit: https://opensource.org/licenses/BSD-3-Clause
+# ******************************************************************************
 
 import numpy as np
 import pandas as pd
