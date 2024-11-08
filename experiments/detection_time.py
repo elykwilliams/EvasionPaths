61653ea384aa80f6f9d56f32535b83a067a014f3
marco-campos <50664758+marco-campos@users.noreply.github.com>
2024-11-07 19:20:43 -0600

    detection_time updates.

--- experiments/detection_time.py
+++ experiments/detection_time.py
@@ -13,6 +13,10 @@ from time_stepping import EvasionPathSimulation
 import numpy as np
 import random
 from tqdm import tqdm
+from itertools import product
+import pickle
+import argparse
+
 
 # seed = 10
 # np.random.seed(seed)
@@ -20,82 +24,97 @@ from tqdm import tqdm
 
 # 69,42 are working
 
-num_sensors: int = 10
-sensing_radius: float = 0.2
-timestep_size: float = 0.05
-sensor_velocity = 1
-n_runs: int = 10
-
-log_name = f"{num_sensors}sensors_{sensing_radius}r_{n_runs}nr"
-log_file = log_name + ".log"
-logging.basicConfig(filename=log_file, level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-domain = UnitCube()
-motion_model = BilliardMotion()
-"""
-Testing fence stuff
-"""
-
-####################### Fence stuff #####################3
-
-# fence = UnitCubeFence(sensing_radius)
-# fence_sensors = [Sensor(point, (0, 0, 0), sensing_radius, True) for point in fence]
-# fence = read_fence("../examples/Fence/fence.csv", sensing_radius)
-fence = get_unitcube_fence(spacing=sensing_radius)
-
-points = domain.point_generator(num_sensors)
-
-output_dir: str = "./output"
-r_string = str(sensing_radius).replace(".", "")
-
-filename_base: str = f"{n_runs}_runs_{num_sensors}s_{r_string}r_10_22"
-
-def simulate() -> float:
-    logging.info("Starting a simulation.")
+def simulate(sim) -> float:
     try:
-
-        mobile_sensors = generate_mobile_sensors(domain, num_sensors, sensing_radius, sensor_velocity)
-        sensor_network = SensorNetwork(
-            mobile_sensors=mobile_sensors,
-            motion_model=motion_model,
-            fence=fence,
-            sensing_radius=sensing_radius,
-            domain=domain
-        )
-        simulation = EvasionPathSimulation(sensor_network, timestep_size)
-        logging.info("Simulation set up")
-        print("Simulation set up")
-        return simulation.run()
+        return sim.run()
     except Exception as e:
-        logging.error(f"Simulation failed due to {e}. Retrying...")
+        print(f"Simulation failed due to {e}. Retrying...")
         return None
 
 
-def output_data(filename: str, data_points: list) -> None:
-    with open(filename, 'a+') as file:
-        for d in data_points:
-            if type(d) != str:
-                file.writelines("%.2f\n" % d)
-            else:
-                file.writelines(str(d) + "\n")
-
-
-def run_experiment() -> None:
+def sim_runs(radius, n_sens, num_runs):
     times = []
-    while len(times) < n_runs:
-        result = simulate()
-        if result is not None:
-            times.append(result)
-        logging.info(f"Simulation completed successfully: {result}. Total completed: {len(times)}/{n_runs}")
-    logging.info(f"All {n_runs} simulations completed.")
-    filename = output_dir + "/" + filename_base + ".txt"
-    output_data(filename, times)
+    while len(times) < num_runs:
+        try:
+            sim = sim_init(n_sensors=n_sens, radii=radius)
+            result = simulate(sim)
+            if result is not None:
+                times.append(result)
+        except Exception as e:
+            print(f"An error occurred: {e}. Retrying initialization...")
+    return times
+
+
+def sim_init(n_sensors, radii):
+    timestep_size: float = 0.05
+    sensor_velocity = 1
+    domain = UnitCube()
+    motion_model = BilliardMotion()
+
+    ####################### Fence stuff #####################3
+
+    # fence = UnitCubeFence(sensing_radius)
+    # fence_sensors = [Sensor(point, (0, 0, 0), sensing_radius, True) for point in fence]
+    # fence = read_fence("../examples/Fence/fence.csv", sensing_radius)
+    fence = get_unitcube_fence(spacing=radii)
+
+    mobile_sensors = generate_mobile_sensors(domain, n_sensors, radii, sensor_velocity)
+    sensor_network = SensorNetwork(
+        mobile_sensors=mobile_sensors,
+        motion_model=motion_model,
+        fence=fence,
+        sensing_radius=radii,
+        domain=domain
+    )
+    sim = EvasionPathSimulation(sensor_network, timestep_size)
+
+    return sim
 
 
 if __name__ == "__main__":
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
 
-    run_experiment()
+    parser = argparse.ArgumentParser(description="Run the atomic change detection simulation with specified parameters.")
+
+    # Define arguments for the main parameters
+    parser.add_argument("--min_sensors", type=int, default=15, help="Min number of mobile sensors")
+    parser.add_argument("--max_sensors", type=int, default=25, help="Max number of mobile sensors")
+    parser.add_argument("--lower_r", type=float, default=0.35, help="Lower bound of sensing radius")
+    parser.add_argument("--upper_r", type=float, default=0.45, help="Upper bound of sensing radius")
+    parser.add_argument("--subdivisions", type=int, default=11, help="Number of subdivisions for sensing radius range")
+    parser.add_argument("--output_dir", type=str, default="./output/Tmax/", help="Directory to save output CSV files")
+    parser.add_argument("--fn", type=str, default="tmax.pkl", help="filename for tmax")
+    parser.add_argument("--n_runs", type=int, default=100, help="Number of simulations")
+
+    args = parser.parse_args()
+
+    min_num_sensors = args.min_sensors
+    max_num_sensors = args.max_sensors
+    min_r = args.lower_r
+    max_r = args.upper_r
+    subdivisions = args.subdivisions
+    output_dir = args.output_dir
+    fn = args.fn
+    n_runs = args.n_runs
+
+    sensing_radii = [round(min_r + i * (max_r - min_r) / (subdivisions - 1), 2) for i in
+                     range(subdivisions)]
+    sensing_radii = sensing_radii[::-1]
+
+    num_sensors = [int(min_num_sensors + i * (max_num_sensors - min_num_sensors) / (subdivisions - 1)) for i in
+                   range(subdivisions)]
+    num_sensors = num_sensors[::-1]
+
+    results_dict = {}
+
+    print(sensing_radii, num_sensors)
+    for r, n in product(sensing_radii, num_sensors):
+        print("Current parameters: ", r, n)
+
+        T_max_arr = sim_runs(r, n, num_runs=n_runs)
+        key = f"{r},{n}"
+        results_dict[key] = T_max_arr
+        print(results_dict)
+
+    with open(fn, 'wb') as f:
+        pickle.dump(output_dir, f)
+
